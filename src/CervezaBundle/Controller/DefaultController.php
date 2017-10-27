c61b7dca1166d89903d20a6f8f393962406db43d
Pablo <pabete.l@gmail.com>
2017-10-27 11:50:29 +0200

    Devolvemos una cerveza

--- src/CervezaBundle/Controller/DefaultController.php
+++ src/CervezaBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace CervezaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use CervezaBundle\Entity\Cerveza;
 
 class DefaultController extends Controller
 {
@@ -20,5 +21,14 @@ class DefaultController extends Controller
     public function cervezasAction()
     {
         return $this->render('CervezaBundle:Default:cervezas.html.twig');
+    }
+    /**
+     * @Route("/cerveza/{id}")
+     */
+    public function cervezaAction($id)
+    {
+      $repository = $this->getDoctrine()->getRepository(Cerveza::class);
+      $cervezas = $repository->find($id);
+      return $this->render('CervezaBundle:Default:cerveza.html.twig', array('cerveza' => $cervezas));
     }
 }
